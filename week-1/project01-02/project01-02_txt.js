5dde3bfc26e3839cc84274417a38ebca65f92475
CXLEB-99 <calebgoforth99@gmail.com>
2024-08-21 17:46:58 -0400

    Week 1 commit

---
+++ week-1/project01-02/project01-02_txt.js
@@ -0,0 +1,12 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author:Caleb Goforth
+    Date:8/21/24
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+let service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate", service1Speed = "0 Mbps", service2Speed = "100 Mpbs", service3Speed = "500 Mbps", service4Speed = "1 Gig"
